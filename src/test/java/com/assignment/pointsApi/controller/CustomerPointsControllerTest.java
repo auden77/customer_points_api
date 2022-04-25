26420a01a62c6614a24bb574adf014d85d5affe4
Eb Auden <eb@anythingland.com>
2022-04-25 14:24:47 -0500

    clean up CustomerPointsControllerTest.java

--- src/test/java/com/assignment/pointsApi/controller/CustomerPointsControllerTest.java
+++ src/test/java/com/assignment/pointsApi/controller/CustomerPointsControllerTest.java
@@ -26,7 +26,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CustomerPointsController.class)
-//@WithMockUser
 public class CustomerPointsControllerTest {
 
     @MockBean
